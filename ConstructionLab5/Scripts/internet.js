b48a4758dab4121a1362a6414f87c3f0fcb1e10b
filon13p <filon13p@gmail.com>
2021-04-08 18:40:58 +0300

    подключил шаблонизацию через  vue

---
+++ ConstructionLab5/Scripts/internet.js
@@ -0,0 +1,14 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+        ,tariff_flats: [
+            { title: 'Квартирный  S', descr: 'Длинное описание тарифа Квартирный S', price: 590 },
+            { title: 'Квартирный  M', descr: 'Длинное описание тарифа Квартирный M', price: 990 },
+            { title: 'Квартирный  L', descr: 'Длинное описание тарифа Квартирный L', price: 1290 },
+
+        ]
+
+    }
+
+});
