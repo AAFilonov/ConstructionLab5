65f36264004d28296aad73a9371bb0e1b44a0b42
filon13p <filon13p@gmail.com>
2021-04-09 09:46:21 +0300

    фвы

---
+++ ConstructionLab5/Controllers/TariffsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+using System.Data.SqlClient;
+
+
+namespace ConstructionLab5.Controllers
+{
+    public class TariffsController : ApiController
+    {
+
+        private static readonly string[] Summaries = new[]
+       {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [Route("api/[controller]/get_tariffs_data")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var actionResult = new Dictionary<string, string> {
+               {  "Квартирный  S","Длинное описание тарифа Квартирный S"},
+            {  "Квартирный  M", "Длинное описание тарифа Квартирный M"},
+            {  "Квартирный  L",  "Длинное описание тарифа Квартирный L" }
+          };
+            return new JsonResult(actionResult);
+
+        }
+    }
+}
---
+++ ConstructionLab5/Models/Tariff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConstructionLab5.Models
+{
+    public class Tariff
+    {
+        
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Descr { get; set; }
+            public int PricePerMounth { get; set; }
+      
+    }
+}
